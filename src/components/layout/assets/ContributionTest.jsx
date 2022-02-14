a437681c7f89fce832444f9a01e38175407c74d3
Daniel Murphy <daniel.murphy@capco.com>
2022-02-14 01:11:41 -0500

    Another contribution test

--- src/components/layout/assets/ContributionTest.jsx
+++ src/components/layout/assets/ContributionTest.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function ContributionTest() {
   return (
-    <div>This is a test ot see if contributions display is updated now</div>
+    <div>
+      This is a test ot see if contributions display is updated now
+      <H3>Another one</H3>
+    </div>
   );
 }
 
