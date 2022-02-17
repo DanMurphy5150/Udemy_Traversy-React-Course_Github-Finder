4a1ab01f5b7d7c672bba71faa153ff24ce589723
Daniel Murphy <daniel.murphy@capco.com>
2022-02-16 23:36:02 -0500

    Added search functionality and clear button functionality to the form

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -15,28 +15,41 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-  // get initial users - testing purposes function
-  const fetchUsers = async () => {
+  // get search results
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
-    const data = await response.json();
+    const { items } = await response.json();
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     });
   };
 
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
+  const clearUsers = () =>
+    dispatch({ type: 'CLEAR_USERS', payload: initialState });
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
